d3786f9ec58bcb7c0d2f191c05ee156fc7f8a10d
IvanProkhorets <prohorets.ivan@gmail.com>
2024-03-24 23:12:40 +0200

    add api (example)

---
+++ src/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.unsplash.com';
+const accessKey = '7XkF714vZ0F0q32qkxaWKIGN6Ojw-1NKMPc0P-krJHY';
+
+export async function fetchPhotos(searchQuery, page = 1) {
+  const response = await axios.get('/search/photos', {
+    headers: {
+      Authorization: `Client-ID ${accessKey}`,
+    },
+    params: {
+      query: searchQuery,
+      per_page: 30,
+      page,
+    },
+  });
+
+  console.log(page);
+
+  return response.data;
+}
