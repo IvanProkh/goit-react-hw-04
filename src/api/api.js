db555b9c26b7b6507da5a74ecee15e5a0415bad9
IvanProkhorets <prohorets.ivan@gmail.com>
2024-03-26 00:47:48 +0200

    update 1.0

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.unsplash.com';
-const accessKey = '7XkF714vZ0F0q32qkxaWKIGN6Ojw-1NKMPc0P-krJHY';
+const accessKey = 'FjEiOy4Ip3d7VQ6fJCKqVBYfnhjcLEd4B1dNou22WyA';
 
 export async function fetchPhotos(searchQuery, page = 1) {
   const response = await axios.get('/search/photos', {
