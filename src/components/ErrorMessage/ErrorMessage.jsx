d6269b1b788f035a5d30cd25392e11ad28d60074
IvanProkhorets <prohorets.ivan@gmail.com>
2024-03-26 16:00:40 +0200

    update 1.0

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,14 @@
+import css from './ErrorMessage.module.css';
+
+export default function ErrorMessage() {
+  return (
+    <div className={css.div}>
+      <h2 className={css.title}>Server Error</h2>
+
+      <p className={css.text}>
+        Unfortunately, there was an error while accessing the server.
+      </p>
+      <h3 className={css.text}>Enter a valid query or reload the page!</h3>
+    </div>
+  );
+}
