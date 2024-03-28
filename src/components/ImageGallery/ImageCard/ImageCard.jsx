a64ce9a807f2fcd2998c15739a99152e5d17de44
IvanProkhorets <prohorets.ivan@gmail.com>
2024-03-28 04:02:35 +0200

    fix

--- src/components/ImageGallery/ImageCard/ImageCard.jsx
+++ src/components/ImageGallery/ImageCard/ImageCard.jsx
@@ -1,8 +1,8 @@
 import css from './ImageCard.module.css';
 
-export default function ImageCard({ cardPhoto }) {
+export default function ImageCard({ cardPhoto, onClick }) {
   return (
-    <div>
+    <div onClick={onClick}>
       <img className={css.img} src={cardPhoto.urls.small} alt="" />
     </div>
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,14 +6,13 @@ const ImageGallery = forwardRef(({ galleryItems, openModal }, ref) => {
   return (
     <ul ref={ref} className={css.ul}>
       {galleryItems.map(photo => (
-        <li
-          onClick={() => {
-            openModal(photo);
-          }}
-          className={css.li}
-          key={photo.id}
-        >
-          <ImageCard cardPhoto={photo}></ImageCard>
+        <li className={css.li} key={photo.id}>
+          <ImageCard
+            cardPhoto={photo}
+            onClick={() => {
+              openModal(photo);
+            }}
+          ></ImageCard>
         </li>
       ))}
     </ul>
